423659ca58fd4e7e3ea0ea691079105434f7b32e
Nicholas Kane <nkanedev@gmail.com>
2020-09-23 08:16:27 -0400

    wip

--- lesson-19/build.bat
+++ lesson-19/build.bat
@@ -1,1 +1,4 @@
-cl /nologo /Zi /MD main.cpp /I..\libs\include /link ..\libs\glfw\lib-vc2019\glfw3.lib ..\libs\glew\glew32s.lib user32.lib gdi32.lib shell32.lib opengl32.lib
+cl /nologo /Zi /MD main.cpp ..\libs\include\imgui\*.cpp /I..\libs\include /link ..\libs\glfw\lib-vc2019\glfw3.lib ..\libs\glew\glew32s.lib user32.lib gdi32.lib shell32.lib opengl32.lib
+::cl /nologo /Zi /MD ..\libs\include\imgui\*.cpp /I..\libs\include /link ..\libs\glfw\lib-vc2019\glfw3.lib ..\libs\glew\glew32s.lib user32.lib gdi32.lib shell32.lib opengl32.lib
+::cp ..\\include\\imgui\\*.obj  .
+::cl /nologo /Zi /MD main.cpp *.obj /I..\libs\include /link ..\libs\glfw\lib-vc2019\glfw3.lib ..\libs\glew\glew32s.lib user32.lib gdi32.lib shell32.lib opengl32.lib
--- lesson-19/main.cpp
+++ lesson-19/main.cpp
@@ -19,6 +19,9 @@
 #include "shader.cpp"
 #include "texture.cpp"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw_gl3.h"
+
 int main()
 {
     GLFWwindow *window;
@@ -108,17 +111,50 @@ int main()
 
     Renderer renderer = {};
 
-    float increment = 0.05f;
+    ImGui::CreateContext();
+    ImGui_ImplGlfwGL3_Init(window, true);
+    ImGui::StyleColorsDark();
+
+    bool show_demo_window = true;
+    bool show_another_window = true;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
     while (!glfwWindowShouldClose(window))
     {
         Clear(&renderer);
+        ImGui_ImplGlfwGL3_NewFrame();
+
         Bind(shader);
         Draw(&renderer, vertexArray, indexBuffer, shader);
+
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+            ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
+
+        ImGui::Render();
+        ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     {
+        ImGui_ImplGlfwGL3_Shutdown();
+        ImGui::DestroyContext();
         DeleteShader(shader);
         DeleteVertexBuffer(vertexBuffer);
         DeleteIndexBuffer(indexBuffer);
