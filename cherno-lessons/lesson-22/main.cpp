4d7b0bd5b76da3cb2d2e22dedaabfdc1a4a3f4f1
Nicholas Kane <nkanedev@gmail.com>
2020-09-30 09:37:34 -0400

    wip

---
+++ cherno-lessons/lesson-22/main.cpp
@@ -0,0 +1,106 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <assert.h>
+#define GLEW_STATIC
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "renderer.cpp"
+#include "vertex_buffer.cpp"
+#include "index_buffer.cpp"
+#include "vertex_array.cpp"
+#include "shader.cpp"
+#include "texture.cpp"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw_gl3.h"
+
+#include "tests/test.cpp"
+
+int main()
+{
+    GLFWwindow *window;
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    if (glewInit() != GLEW_OK)
+    {
+        printf("Error: failed to init glew\n");
+    }
+    printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
+
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC1_ALPHA));
+    GLCall(glEnable(GL_BLEND));
+
+    Renderer renderer = {};
+
+    ImGui::CreateContext();
+    ImGui_ImplGlfwGL3_Init(window, true);
+    ImGui::StyleColorsDark();
+
+    Test_Clear_Color clear_color_test = {};
+    clear_color_test.r = 1.0f;
+    clear_color_test.g = 0.0f;
+    clear_color_test.b = 0.0f;
+    clear_color_test.a = 1.0f;
+
+    Test_Texture_2D *texture_2D_test = NULL;
+
+    Test_Main_Menu menu = {};
+    menu.Tests[1] = (unsigned long int*)&clear_color_test;
+    menu.TestType[1] = CLEAR_COLOR;
+
+    menu.Tests[2] = (unsigned long int*)texture_2D_test;
+    menu.TestType[2] = TEXTURE_2D;
+
+    menu.CurrentTestIndex = 0;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
+        Clear(&renderer);
+
+        OnUpdate_Test_Menu(&menu);
+        OnRender_Test_Menu(&menu);
+
+        ImGui_ImplGlfwGL3_NewFrame();
+        OnImGuiRender_Test_Menu(&menu);
+
+        ImGui::Render();
+        ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    {
+        ImGui_ImplGlfwGL3_Shutdown();
+        ImGui::DestroyContext();
+        glfwTerminate();
+    }
+
+    return 0;
+}
---
+++ cherno-lessons/lesson-22/tests/test.cpp
@@ -0,0 +1,286 @@
+#include "test.h"
+#include "renderer.h"
+#include "imgui/imgui.h"
+
+#include <GL/glew.h>
+
+void
+OnUpdate_Test_Menu(Test_Main_Menu *m)
+{
+
+}
+
+void 
+OnRender_Test_Menu(Test_Main_Menu *m)
+{
+
+}
+
+void 
+OnImGuiRender_Test_Menu(Test_Main_Menu *s)
+{
+    unsigned int i = s->CurrentTestIndex;
+    unsigned int type = s->TestType[i];
+    unsigned long int *test = s->Tests[i];
+    switch (type)
+    {
+        case CLEAR_COLOR:
+        {
+            Test_Clear_Color *t = (Test_Clear_Color *)test;
+            OnUpdate_Clear_Color(t, 0.0f);
+            OnRender_Clear_Color(t);
+            ImGui::Begin("Test");
+            if (ImGui::Button("Back"))
+            {
+                s->CurrentTestIndex = MENU;
+            }
+            OnImGuiRender_Clear_Color(t);
+            ImGui::End();
+        } break;
+
+        case TEXTURE_2D:
+        {
+            if (s->Tests[i] == NULL)
+            {
+                s->Tests[i] = (unsigned long int *)Create_Test_Texture_2D();   
+                test = s->Tests[i];
+            }
+            Test_Texture_2D *t = (Test_Texture_2D *)test;
+            OnUpdate_Texture_2D(t);
+            OnRender_Texture_2D(t);
+            ImGui::Begin("Test");
+            if (ImGui::Button("Back"))
+            {
+                s->CurrentTestIndex = MENU;
+                DeleteShader(t->Shader);
+                DeleteVertexBuffer(t->VertexBuffer);
+                DeleteIndexBuffer(t->IndexBuffer);
+                DeleteVertexArray(t->VertexArray);
+                DeleteTexture(t->Texture);
+                free(t->VertexBuffer);
+                free(t->IndexBuffer);
+                free(t->VertexArray);
+                free(t->VertexBufferLayout->Elements);
+                free(t->VertexBufferLayout);
+                free(t->Texture);
+                free(s->Tests[i]);
+                s->Tests[i] = NULL;
+            }
+            OnImGuiRender_Texture_2D(t);
+            ImGui::End();
+        } break;
+
+        default:
+        {
+            if (ImGui::Button("Clear Color Test"))
+            {
+                s->CurrentTestIndex = CLEAR_COLOR;
+            }
+            if (ImGui::Button("Texture 2D Test"))
+            {
+                s->CurrentTestIndex = TEXTURE_2D;
+            }
+        };
+    }
+}
+
+void
+OnUpdate_Clear_Color(Test_Clear_Color *t, float deltaTime)
+{
+
+}
+
+Test_Texture_2D *
+Create_Test_Texture_2D()
+{
+    Test_Texture_2D *Result = NULL;
+    Result = (Test_Texture_2D *)malloc(sizeof(Test_Texture_2D));
+    if (Result == NULL)
+    {
+        return NULL;
+    }
+    Result->TranslationA = glm::vec3(200.0f, 200.0f, 0.0f);
+    Result->TranslationB = glm::vec3(400.0f, 200.0f, 0.0f);
+
+    float vertexBufferData[16] = 
+    {
+         -50.0f, -50.0f, 0.0f, 0.0f,  // 0
+          50.0f, -50.0f, 1.0f, 0.0f,  // 1
+          50.0f,  50.0f, 1.0f, 1.0f,  // 2
+         -50.0f,  50.0f, 0.0f, 1.0f,  // 3
+    };
+
+    unsigned int indexBufferData[6] = 
+    {
+        0, 1, 2, // right lower triangle
+        2, 3, 0, // left upper triangle
+    };
+
+    GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC1_ALPHA));
+    GLCall(glEnable(GL_BLEND));
+
+    VertexArray *vertexArray = NewVertexArray();
+    ASSERT(vertexArray != NULL);
+    Result->VertexArray = vertexArray;
+
+    VertexBuffer *vertexBuffer = NewVertexBuffer(vertexBufferData, 16 * sizeof(float));
+    ASSERT(vertexBuffer != NULL);
+    Result->VertexBuffer = vertexBuffer;
+
+    VertexBufferLayout *vertexBufferLayout = NewVertexBufferLayout(2);
+    ASSERT(vertexBufferLayout != NULL);
+    Result->VertexBufferLayout = vertexBufferLayout;
+    Push(vertexBufferLayout, GL_FLOAT, 2);
+    Push(vertexBufferLayout, GL_FLOAT, 2);
+    AddBuffer(vertexArray, vertexBuffer, vertexBufferLayout);
+
+    IndexBuffer *indexBuffer = NewIndexBuffer(indexBufferData, 6);
+    Result->IndexBuffer = indexBuffer;
+    ASSERT(indexBuffer != NULL);
+
+    // projection matrix orthogonal
+    Result->ProjectionMatrix = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+    // view matrix (identity matrix) with translation
+    Result->ViewMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+
+    const char *vertexShaderPath = "shaders/vertex.glsl";
+    const char *fragmentShaderPath = "shaders/fragment.glsl";
+    Shader *shader = NewShader(vertexShaderPath, strlen(vertexShaderPath), fragmentShaderPath, strlen(fragmentShaderPath));
+    ASSERT(shader != NULL);
+    Result->Shader = shader;
+    Bind(shader);
+
+    Texture *texture = NewTexture("..\\assets\\image.png");
+    ASSERT(texture != NULL);
+    Result->Texture = texture;
+    Bind(texture, 0);
+    SetUniform1i(shader, "u_texture", 0);
+
+    Unbind(vertexArray);
+    Unbind(vertexBuffer);
+    Unbind(indexBuffer);
+    Unbind(shader);
+
+    return Result;
+}
+
+void
+OnRender_Clear_Color(Test_Clear_Color *t)
+{
+    GLCall(glClearColor(t->r, t->b, t->g, t->a));
+    GLCall(glClear(GL_COLOR_BUFFER_BIT));
+}
+
+void
+OnImGuiRender_Clear_Color(Test_Clear_Color *t)
+{
+    ImGui::ColorEdit4("Clear Color", t->Color);
+}
+
+void
+OnUpdate_Texture_2D(Test_Texture_2D *t)
+{
+
+}
+
+void
+OnRender_Texture_2D(Test_Texture_2D *t)
+{
+    GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
+    GLCall(glClear(GL_COLOR_BUFFER_BIT));
+
+    glm::mat4 modelMatrix = glm::translate(glm::mat4(1.0f), t->TranslationA);
+    glm::mat4 modelViewProjectionMatrix = t->ProjectionMatrix * t->ViewMatrix * modelMatrix;
+    Bind(t->Shader);
+    SetUniformMatrix4f(t->Shader, "u_model_view_projection_matrix", modelViewProjectionMatrix);
+    Renderer renderer = {};
+    Draw(&renderer, t->VertexArray, t->IndexBuffer, t->Shader);
+
+    modelMatrix = glm::translate(glm::mat4(1.0f), t->TranslationB);
+    modelViewProjectionMatrix = t->ProjectionMatrix * t->ViewMatrix * modelMatrix;
+    Bind(t->Shader);
+    SetUniformMatrix4f(t->Shader, "u_model_view_projection_matrix", modelViewProjectionMatrix);
+    Draw(&renderer, t->VertexArray, t->IndexBuffer, t->Shader);
+
+}
+
+void
+OnImGuiRender_Texture_2D(Test_Texture_2D *t)
+{
+    ImGui::SliderFloat3("Translation A", &t->TranslationA.x, 0.0f, 960.0f);
+    ImGui::SliderFloat3("Translation B", &t->TranslationB.x, 0.0f, 960.0f);
+    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+}
+
+Test_Quad *
+Create_Test_Quad()
+{
+    Test_Quad *Result = NULL;
+    Result = (Test_Quad *)malloc(sizeof(Test_Quad));
+    if (Result == NULL)
+    {
+        return NULL;
+    }
+
+    Result->Vertices[0] = -0.5f;
+    Result->Vertices[1] = -0.5f;
+    Result->Vertices[2] =  0.0f;
+
+    Result->Vertices[3] =  0.5f;
+    Result->Vertices[4] = -0.5f;
+    Result->Vertices[5] =  0.0f;
+
+    Result->Vertices[6] =  0.5f;
+    Result->Vertices[7] =  0.5f;
+    Result->Vertices[8] =  0.0f;
+
+    Result->Vertices[9] = -0.5f;
+    Result->Vertices[10] =  0.5f;
+    Result->Vertices[11] =  0.0f;
+
+    Result->VertexArray = NewVertexArray();
+    ASSERT(Result->VertexArray != NULL);
+
+    Result->VertexBuffer = NewVertexBuffer(Result->Vertices, 12 * sizeof(float));
+    ASSERT(Result->VertexBuffer != NULL);
+
+    Result->VertexBufferLayout = NewVertexBufferLayout(2);
+    ASSERT(Result->VertexBufferLayout != NULL);
+    Push(Result->VertexBufferLayout, GL_FLOAT, 3);
+
+    AddBuffer(Result->VertexArray, Result->VertexBuffer, Result->VertexBufferLayout);
+
+    // right lower triangle
+    Result->Indices[0] = 0;
+    Result->Indices[1] = 1;
+    Result->Indices[2] = 2;
+
+    // left upper triangle
+    Result->Indices[3] = 2;
+    Result->Indices[4] = 3;
+    Result->Indices[5] = 0;
+
+    Result->IndexBuffer = NewIndexBuffer(Result->Indices, 6);
+    ASSERT(Result->IndexBuffer != NULL);
+
+    return Result;
+}
+
+void
+OnUpdate_Test_Quad(Test_Quad *t)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram();
+}
+
+void
+OnRender_Test_Quad(Test_Quad *t)
+{
+
+}
+
+void
+OnImGuiRender_Test_Quad(Test_Quad *t)
+{
+
+}
