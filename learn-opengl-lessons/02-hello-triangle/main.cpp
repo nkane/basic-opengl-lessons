04e83abfef776c4aa6103ac7164fec54a98b9742
Nick Kane <nkanedev@gmail.com>
2020-10-01 09:00:22 -0400

    wip

--- learn-opengl-lessons/02-hello-triangle/main.cpp
+++ learn-opengl-lessons/02-hello-triangle/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <memory.h>
 
 #define GLEW_STATIC
 #include <GL\glew.h>
@@ -81,7 +82,46 @@ main()
     {
         glGetShaderInfoLog(vertex_shader_id, 512, NULL, info_logs);
         printf("[Error - Vertex Shader]: %s\n", info_logs);
+        return -1;
     }
+
+    memset(info_logs, 0, 512);
+
+    const char *fragment_shader_source =
+    "#version 330 core\n"
+    "out vec4 color;\n"
+    "void main()\n"
+    "{\n"
+    "   color = vec4(1.0, 0.5, 0.2, 1.0f);\n"
+    "}\0";
+    unsigned int fragment_shader_id;
+    fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader_id, 1, &fragment_shader_source, NULL);
+    glCompileShader(fragment_shader_id);
+    glGetShaderiv(fragment_shader_id, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragment_shader_id, 512, NULL, info_logs);
+        printf("[Error - Fragment Shader]: %s\n", info_logs);
+    }
+
+    memset(info_logs, 0, 512);
+
+    unsigned int shader_program_id;
+    shader_program_id = glCreateProgram();
+    glAttachShader(shader_program_id, vertex_shader_id);
+    glAttachShader(shader_program_id, fragment_shader_id);
+    glLinkProgram(shader_program_id);
+    glGetProgramiv(shader_program_id, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shader_program_id, 512, NULL, info_logs);
+        printf("[Error - Program Linking]: %s\n", info_logs);
+    }
+
+    glUseProgram(shader_program_id);
+    glDeleteShader(vertex_shader_id);
+    glDeleteShader(fragment_shader_id);
 
     while (!glfwWindowShouldClose(window))
     {
