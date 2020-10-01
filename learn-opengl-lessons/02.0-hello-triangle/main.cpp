6dbbb793d33d54a4ed60bc34d3b4f20255e792d2
Nick Kane <nkanedev@gmail.com>
2020-10-01 12:17:46 -0400

    wip

--- learn-opengl-lessons/02.0-hello-triangle/main.cpp
+++ learn-opengl-lessons/02.0-hello-triangle/main.cpp
@@ -5,6 +5,8 @@
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
 
+static unsigned int WireFrameEnabled = 0;
+
 void
 framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
@@ -17,6 +19,11 @@ process_input(GLFWwindow *window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) ==  GLFW_PRESS)
     {
         glfwSetWindowShouldClose(window, true);
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        WireFrameEnabled = ~WireFrameEnabled;
     }
 }
 
@@ -51,18 +58,40 @@ main()
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    float vertices[9] =
-    {
-        -0.5f, -0.5f, 0.0f,
-         0.5f,  0.0f, 0.0f,
-         0.0f,  0.5f, 0.0f,
-    };
+    // bind vertex array
+    unsigned int vertex_array_id;
+    glGenVertexArrays(1, &vertex_array_id);
+    glBindVertexArray(vertex_array_id);
 
+    // copy vertices array in a vertex buffer
+    float vertices[12] =
+    {
+         0.5f,  0.5f,  0.0f,     // top right
+         0.5f, -0.5f,  0.0f,     // bottom right
+        -0.5f, -0.5f,  0.0f,     // bottom left
+        -0.5f,  0.5f,  0.0f,     // top left
+    };
     unsigned vertex_buffer_id;
     glGenBuffers(1, &vertex_buffer_id);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_id);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    // copy index array in an element buffer (index buffer)
+    unsigned int indices[6] =
+    {
+        0, 1, 3,    // first triangle
+        1, 2, 3,    // second triangle
+    };
+    unsigned int index_buffer_id;
+    glGenBuffers(1, &index_buffer_id);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer_id);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // set vertex arrtribute pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    // create vertex shader
     const char *vertex_shader_source = 
     "#version 330 core\n"
     "layout (location = 0) in vec3 position;\n"
@@ -74,7 +103,6 @@ main()
     vertex_shader_id = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader_id, 1, &vertex_shader_source, NULL);
     glCompileShader(vertex_shader_id);
-
     int success;
     char info_logs[512];
     glGetShaderiv(vertex_shader_id, GL_COMPILE_STATUS, &success);
@@ -84,9 +112,9 @@ main()
         printf("[Error - Vertex Shader]: %s\n", info_logs);
         return -1;
     }
-
     memset(info_logs, 0, 512);
 
+    // create fragment shader
     const char *fragment_shader_source =
     "#version 330 core\n"
     "out vec4 color;\n"
@@ -104,9 +132,9 @@ main()
         glGetShaderInfoLog(fragment_shader_id, 512, NULL, info_logs);
         printf("[Error - Fragment Shader]: %s\n", info_logs);
     }
-
     memset(info_logs, 0, 512);
 
+    // create shader program linking vertex and fragment shader
     unsigned int shader_program_id;
     shader_program_id = glCreateProgram();
     glAttachShader(shader_program_id, vertex_shader_id);
@@ -118,10 +146,14 @@ main()
         glGetProgramInfoLog(shader_program_id, 512, NULL, info_logs);
         printf("[Error - Program Linking]: %s\n", info_logs);
     }
+    memset(info_logs, 0, 512);
+    {
+        // shaders no longer needed, since linked to program
+        glDeleteShader(vertex_shader_id);
+        glDeleteShader(fragment_shader_id);
+    }
 
-    glUseProgram(shader_program_id);
-    glDeleteShader(vertex_shader_id);
-    glDeleteShader(fragment_shader_id);
+    glBindVertexArray(0);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -129,12 +161,31 @@ main()
         {
             glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
+
+            if (WireFrameEnabled)
+            {
+                glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+            }
+            else
+            {
+                glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            }
+
+            glUseProgram(shader_program_id);
+            glBindVertexArray(vertex_array_id);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         }
         glfwPollEvents();
         glfwSwapBuffers(window);
     }
     
-    glfwTerminate();
+    {
+        glDeleteVertexArrays(1, &vertex_array_id);
+        glDeleteBuffers(1, &vertex_buffer_id);
+        glDeleteBuffers(1, &index_buffer_id);
+        glDeleteProgram(shader_program_id);
+        glfwTerminate();
+    }
     
     return 0;
 }
