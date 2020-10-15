09167e31a488f832eddf331ff936d534e7d64181
Nick Kane <nkanedev@gmail.com>
2020-10-14 21:49:54 -0400

    wip

--- learn-opengl-lessons/07.6-camera-exercise/camera.cpp
+++ learn-opengl-lessons/07.6-camera-exercise/camera.cpp
@@ -1,5 +1,6 @@
 #define GLEW_STATIC
 #include <GL\glew.h>
+#include <GLFW\glfw3.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -31,6 +32,29 @@ struct Camera
     float mouse_sensitivity;
     float zoom;
 };
+
+glm::mat4
+LookAt(glm::vec3 position, glm::vec3 target, glm::vec3 world_up)
+{
+    glm::vec3 z_axis = glm::normalize(position - target);
+    glm::vec3 x_axis = glm::normalize(glm::cross(glm::normalize(world_up), z_axis));
+    glm::vec3 y_axis = glm::cross(z_axis, x_axis);
+    glm::mat4 translation = glm::mat4(1.0f);
+    translation[3][0] = -position.x;
+    translation[3][1] = -position.y;
+    translation[3][2] = -position.z;
+    glm::mat4 rotation = glm::mat4(1.0f);
+    rotation[0][0] = x_axis.x;
+    rotation[1][0] = x_axis.y;
+    rotation[2][0] = x_axis.z;
+    rotation[0][1] = y_axis.x;
+    rotation[1][1] = y_axis.y;
+    rotation[2][1] = y_axis.z;
+    rotation[0][2] = z_axis.x;
+    rotation[1][2] = z_axis.y;
+    rotation[2][2] = z_axis.z;
+    return rotation * translation;
+}
 
 void
 UpdateCameraVectors(Camera *c)
@@ -68,6 +92,15 @@ glm::mat4
 GetViewMatrix(Camera *c)
 {
     return glm::lookAt(c->position, c->position + c->front, c->up);
+}
+
+glm::mat4
+CustomGetViewMatrix(Camera *c)
+{
+    const float radius = 10.0f;
+    float cam_x = sin(glfwGetTime()) * radius;
+    float cam_z = cos(glfwGetTime()) * radius;
+    return LookAt(glm::vec3(cam_x, 0.0f, cam_z), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 }
 
 void
