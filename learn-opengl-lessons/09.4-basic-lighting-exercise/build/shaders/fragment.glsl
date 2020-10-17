36186031cc0746b0d3ccf83a90bbfec96fc3ddfa
Nick Kane <nkanedev@gmail.com>
2020-10-17 18:05:53 -0400

    wip

--- learn-opengl-lessons/09.4-basic-lighting-exercise/build/shaders/fragment.glsl
+++ learn-opengl-lessons/09.4-basic-lighting-exercise/build/shaders/fragment.glsl
@@ -4,27 +4,29 @@ out vec4 frag_color;
 
 in vec3 v_fragment_position;
 in vec3 v_normal;
+in vec3 v_light_position;
 
 uniform vec3 u_object_color;
 uniform vec3 u_light_color;
-uniform vec3 u_light_position;
-uniform vec3 u_view_position;
 
 void main()
 {
-    vec3 normal = normalize(v_normal);
-    vec3 light_direction = normalize(u_light_position - v_fragment_position);
-    float diff = max(dot(normal, light_direction), 0.0);
-    vec3 diffuse_light = diff * u_light_color;
-
-    float specular_strength = 0.5;
-    vec3 view_direction = normalize(u_view_position - v_fragment_position);
-    vec3 reflect_direction = reflect(-light_direction, normal);
-    float specular = pow(max(dot(view_direction, reflect_direction), 0.0), 32);
-    vec3 specular_highlight = specular_strength * specular * u_light_color;
-
+    // ambient light
     float ambient_strength = 0.1;
     vec3 ambient_light = ambient_strength * u_light_color;
+
+    // diffuse
+    vec3 norm = normalize(v_normal);
+    vec3 light_direction = normalize(v_light_position - v_fragment_position);
+    float diff = max(dot(norm, light_direction), 0.0);
+    vec3 diffuse_light = diff * u_light_color;
+
+    // specular
+    float specular_strength = 0.5;
+    vec3 view_direction = normalize(-v_fragment_position);
+    vec3 reflection_direction = reflect(-light_direction, norm);
+    float spec = pow(max(dot(view_direction, reflection_direction), 0.0), 32);
+    vec3 specular_highlight = specular_strength * spec * u_light_color;
 
     vec3 result = (ambient_light + diffuse_light + specular_highlight) * u_object_color;
     frag_color = vec4(result, 1.0);
--- learn-opengl-lessons/09.4-basic-lighting-exercise/build/shaders/vertex.glsl
+++ learn-opengl-lessons/09.4-basic-lighting-exercise/build/shaders/vertex.glsl
@@ -5,7 +5,9 @@ layout (location = 1) in vec3 normal;
 
 out vec3 v_fragment_position;
 out vec3 v_normal;
+out vec3 v_light_position;
 
+uniform vec3 u_light_position;
 uniform mat4 u_model;
 uniform mat4 u_view;
 uniform mat4 u_projection;
@@ -13,6 +15,8 @@ uniform mat4 u_projection;
 void main()
 {
    gl_Position = u_projection * u_view * u_model * vec4(position, 1.0);
-   v_normal = normal;
-   v_fragment_position = vec3(u_model * vec4(position, 1.0));
+   v_fragment_position = vec3(u_view * u_model * vec4(position, 1.0));
+   v_normal = mat3(transpose(inverse(u_view * u_model))) * normal;
+   // transform world-space light position to view-space ligth position
+   v_light_position = vec3(u_view * vec4(u_light_position, 1.0));
 }
