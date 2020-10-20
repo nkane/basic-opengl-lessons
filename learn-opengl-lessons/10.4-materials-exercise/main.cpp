202081bd524449e45ab9f270cd3dc8a80043c79b
Nick Kane <nkanedev@gmail.com>
2020-10-20 11:32:15 -0400

    wip

--- learn-opengl-lessons/10.4-materials-exercise/main.cpp
+++ learn-opengl-lessons/10.4-materials-exercise/main.cpp
@@ -214,7 +214,7 @@ main()
     glBindVertexArray(0);
     glEnable(GL_DEPTH_TEST);
 
-    glm::vec3 light_position(0.0f, 2.0f, 1.5f);
+    glm::vec3 light_position(0.0f, -1.0f, 1.5f);
     camera = CreateCamera(camera_position, camera_up, yaw, pitch);
 
     glm::vec3 cube_positions[] =
@@ -248,12 +248,197 @@ main()
 
     Material materials[] = 
     {
+        // emerald
         Material 
         {
             glm::vec3(0.0215f, 0.1745f, 0.0215f),
             glm::vec3(0.07568f,	0.61424f, 0.07568f),
             glm::vec3(0.633f, 0.727811f, 0.633f),
-            32.0f,
+            0.6f * 128.0f,
+        },
+        // jade
+        Material 
+        {
+            glm::vec3(0.135f, 0.2225f, 0.1575f),
+            glm::vec3(0.54f, 0.89f,	0.63f),
+            glm::vec3(0.316228f, 0.316228f,	0.316228f),
+            0.1f * 128.0f,
+        },
+        // obsidian
+        Material 
+        {
+            glm::vec3(0.05375f, 0.05f, 0.06625f),
+            glm::vec3(0.18275f,	0.17f, 0.22525f),
+            glm::vec3(0.332741f, 0.328634f,	0.346435f),
+            0.3f * 128.0f,
+        },
+        // pearl
+        Material 
+        {
+            glm::vec3(0.25f, 0.20725f, 0.20725f),
+            glm::vec3(1.0f,	0.829f,	0.829f),
+            glm::vec3(0.296648f, 0.296648f,	0.296648f),
+            0.088f * 128.0f,
+        },
+        // ruby
+        Material 
+        {
+            glm::vec3(0.1745f, 0.01175f, 0.01175f),
+            glm::vec3(0.61424f,	0.04136f, 0.04136f),
+            glm::vec3(0.727811f, 0.626959f,	0.626959f),
+            0.6f * 128.0f,
+        },
+        // turquoise
+        Material 
+        {
+            glm::vec3(0.1f,	0.18725f, 0.1745f),
+            glm::vec3(0.396f, 0.74151f,	0.69102f),
+            glm::vec3(0.297254f, 0.30829f, 0.306678f),
+            0.1f * 128.0f,
+        },
+        // brass
+        Material 
+        {
+            glm::vec3(0.329412f, 0.223529f,	0.027451f),
+            glm::vec3(0.780392f, 0.568627f,	0.113725f),
+            glm::vec3(0.992157f, 0.941176f, 0.807843f),
+            0.21794872f * 128.0f,
+        },
+        // bronze
+        Material 
+        {
+            glm::vec3(0.2125f, 0.1275f,	0.054f),
+            glm::vec3(0.714f, 0.4284f, 0.18144f),
+            glm::vec3(0.393548f, 0.271906f,	0.166721f),
+            0.2f * 128.0f,
+        },
+        // chrome
+        Material 
+        {
+            glm::vec3(0.25f, 0.25f,	0.25f),
+            glm::vec3(0.4f,	0.4f, 0.4f),
+            glm::vec3(0.774597f, 0.774597f,	0.774597f),
+            0.6f * 128.0f,
+        },
+        // copper 
+        Material 
+        {
+            glm::vec3(0.19125f,	0.0735f, 0.0225f),
+            glm::vec3(0.7038f, 0.27048f, 0.0828f),
+            glm::vec3(0.256777f, 0.137622f, 0.086014f),
+            0.1f * 128.0f,
+        },
+        // gold 
+        Material 
+        {
+            glm::vec3(0.24725f,	0.1995f, 0.0745f),
+            glm::vec3(0.75164f,	0.60648f, 0.22648f),
+            glm::vec3(0.628281f, 0.555802f, 0.366065f),
+            0.4f * 128.0f,
+        },
+        // silver
+        Material 
+        {
+            glm::vec3(0.19225f, 0.19225f, 0.19225f),
+            glm::vec3(0.50754f,	0.50754f, 0.50754f),
+            glm::vec3(0.508273f, 0.508273f,	0.508273f),
+            0.4f * 128.0f,
+        },
+        // black plastic
+        Material 
+        {
+            glm::vec3(0.0f, 0.0f, 0.0f),
+            glm::vec3(0.01f, 0.01f,	0.01f),
+            glm::vec3(0.50f, 0.50f, 0.50f),
+            0.25f * 128.0f,
+        },
+        // cyan plastic
+        Material 
+        {
+            glm::vec3(0.0f,	0.1f, 0.06f),
+            glm::vec3(0.0f,	0.50980392f, 0.50980392f),
+            glm::vec3(0.50196078f, 0.50196078f, 0.50196078f),
+            0.25f * 128.0f,
+        },
+        // green plastic
+        Material 
+        {
+            glm::vec3(0.0f,	0.0f, 0.0f),
+            glm::vec3(0.1f, 0.3f, 0.1f),
+            glm::vec3(0.45f, 0.55f, 0.45f),
+            0.25f * 128.0f,
+        },
+        // red plastic
+        Material 
+        {
+            glm::vec3(0.0f,	0.0f, 0.0f),
+            glm::vec3(0.5f,	0.0f, 0.0f),
+            glm::vec3(0.7f,	0.6f, 0.6f),
+            0.25f * 128.0f,
+        },
+        // white plastic
+        Material 
+        {
+            glm::vec3(0.0f,	0.0f, 0.0f),
+            glm::vec3(0.55f, 0.55f, 0.55f),
+            glm::vec3(0.70f, 0.70f, 0.70f),
+            0.25f * 128.0f,
+        },
+        // yellow plastic
+        Material 
+        {
+            glm::vec3(0.0f,	0.0f, 0.0f),
+            glm::vec3(0.5f, 0.5f, 0.0f),
+            glm::vec3(0.60f, 0.60f, 0.50f),
+            0.25f * 128.0f,
+        },
+        // black rubber
+        Material 
+        {
+            glm::vec3(0.02f, 0.02f, 0.02f),
+            glm::vec3(0.01f, 0.01f, 0.01f),
+            glm::vec3(0.4f, 0.4f, 0.4f),
+            0.78125f * 128.0f,
+        },
+        // cyan rubber
+        Material 
+        {
+            glm::vec3(0.0f, 0.05f, 0.05f),
+            glm::vec3(0.4f, 0.5f, 0.5f),
+            glm::vec3(0.04f, 0.7f, 0.7f),
+            0.78125f * 128.0f,
+        },
+        // green rubber
+        Material 
+        {
+            glm::vec3(0.0f, 0.05f, 0.0f),
+            glm::vec3(0.4f, 0.5f, 0.4f),
+            glm::vec3(0.04f, 0.7f, 0.04f),
+            0.78125f * 128.0f,
+        },
+        // red rubber
+        Material 
+        {
+            glm::vec3(0.05f, 0.0f, 0.0f),
+            glm::vec3(0.5f, 0.4f, 0.4f),
+            glm::vec3(0.7f, 0.04f, 0.04f),
+            0.78125f * 128.0f,
+        },
+        // white rubber
+        Material 
+        {
+            glm::vec3(0.05f, 0.05f, 0.05f),
+            glm::vec3(0.5f, 0.5f, 0.5f),
+            glm::vec3(0.7f, 0.7f, 0.7f),
+            0.78125f * 128.0f,
+        },
+        // yellow rubber
+        Material 
+        {
+            glm::vec3(0.05f, 0.05f, 0.0f),
+            glm::vec3(0.5f, 0.5f, 0.4f),
+            glm::vec3(0.7f,	0.7f, 0.04f),
+            0.78125f * 128.0f,
         },
     };
 
@@ -281,6 +466,14 @@ main()
             glUseProgram(light_shader_program->id);
             glBindVertexArray(vertex_array_light_id);
             model = glm::mat4(1.0f);
+            static float x = 0.0f;
+            static float increment_amount = -2.0f;
+            x += increment_amount * delta_time;
+            if (x < -12.0f || x > 14.0f)
+            {
+                increment_amount *= -1.0f;
+            }
+            light_position.x = x;
             model = glm::translate(model, light_position);
             model = glm::scale(model, glm::vec3(0.2f));
             SetFloatMat4Uniform(light_shader_program, "u_model", glm::value_ptr(model));
@@ -291,12 +484,12 @@ main()
             // render cubes
             glUseProgram(shader_program->id);
             glBindVertexArray(vertex_array_id);
-            for (int i = 0; i < sizeof(cube_positions) / sizeof(glm::vec3); i++) 
+            for (int i = 0; i < sizeof(materials) / sizeof(Material); i++) 
             {
-                SetFloatVec3Uniform(shader_program, "u_material.ambient_light", materials[0].ambient);
-                SetFloatVec3Uniform(shader_program, "u_material.diffuse_light", materials[0].diffuse);
-                SetFloatVec3Uniform(shader_program, "u_material.specular_highlight", materials[0].specular);
-                SetFloatUniform(shader_program, "u_material.shininess", materials[0].shininess);
+                SetFloatVec3Uniform(shader_program, "u_material.ambient_light", materials[i].ambient);
+                SetFloatVec3Uniform(shader_program, "u_material.diffuse_light", materials[i].diffuse);
+                SetFloatVec3Uniform(shader_program, "u_material.specular_highlight", materials[i].specular);
+                SetFloatUniform(shader_program, "u_material.shininess", materials[i].shininess);
 
                 model = glm::mat4(1.0f);
                 model = glm::translate(model, cube_positions[i]);
