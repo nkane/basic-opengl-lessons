d2bb2e1e1dfb077e2f43ccc667a20c179994a978
Nicholas Kane <nkanedev@gmail.com>
2020-10-24 06:14:36 -0400

    wip

---
+++ learn-opengl-lessons/11.2-lighting-maps-exercise/build/shaders/fragment.glsl
@@ -0,0 +1,47 @@
+#version 330 core
+
+struct Material 
+{
+    sampler2D diffuse;
+    sampler2D specular;
+    float     shininess;
+};
+
+struct Light
+{
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+out vec4 v_frag_color;
+
+in vec3 v_fragment_position;
+in vec3 v_normal;
+in vec2 v_texture_coordinates;
+
+uniform vec3 u_view_position;
+uniform Material u_material;
+uniform Light u_light;
+
+void main()
+{
+    // ambient
+    vec3 ambient = u_light.ambient * vec3(texture(u_material.diffuse, v_texture_coordinates));
+
+    // diffuse
+    vec3 norm = normalize(v_normal);
+    vec3 light_directoin = normalize(u_light.position - v_fragment_position);
+    float diff = max(dot(norm, light_directoin), 0.0);
+    vec3 diffuse = u_light.diffuse * diff * vec3(texture(u_material.diffuse, v_texture_coordinates));
+
+    // specular
+    vec3 view_direction = normalize(u_view_position - v_fragment_position);
+    vec3 reflection_direction = reflect(-light_directoin, norm);
+    float spec = pow(max(dot(view_direction, reflection_direction), 0.0), u_material.shininess);
+    vec3 specular = u_light.specular * spec * vec3(texture(u_material.specular, v_texture_coordinates));
+
+    vec3 result = ambient + diffuse + specular;
+    v_frag_color = vec4(result, 1.0);
+}
---
+++ learn-opengl-lessons/11.3-lighting-maps-exercise/main.cpp
@@ -0,0 +1,357 @@
+/*
+ * Excercise 2
+ * - Try inverting the color values of the specular map in the fragment shader
+ *   so that the wood shows specular highlights and the steel borders do not
+ *   (note that due to the cracks in the steel border the borders still show
+ *   some specular highlight, although with less intensity)
+ */
+
+#include <stdio.h>
+#include <memory.h>
+#include <math.h>
+
+#include "shader.cpp"
+#include "camera.cpp"
+#include "material.cpp"
+#include "texture.cpp"
+
+#define GLEW_STATIC
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb\stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+static unsigned int WireFrameEnabled = 0;
+static unsigned int width = 800;
+static unsigned int height = 600;
+
+static Camera *camera = NULL;
+static glm::vec3 camera_position   = glm::vec3(0.0f, 0.0f, 25.0f);
+static glm::vec3 camera_up         = glm::vec3(0.0f, 1.0f, 0.0f);
+static float delta_time = 0.0f;
+static float last_frame = 0.0f;
+
+static float yaw = -90.0f;
+static float pitch = 0.0f;
+static glm::vec3 direction;
+
+void
+framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void
+process_input(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) ==  GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS)
+    {
+        WireFrameEnabled = ~WireFrameEnabled;
+    }
+
+    const float camera_speed = 2.5f * delta_time;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        ProcessKeyboard(camera, FORWARD, delta_time);
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        ProcessKeyboard(camera, BACKWARD, delta_time);
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        ProcessKeyboard(camera, LEFT, delta_time);
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        ProcessKeyboard(camera, RIGHT, delta_time);
+    }
+}
+
+void
+mouse_callback(GLFWwindow *window, double x, double y)
+{
+    static bool first_mouse = true;
+    static float last_x = 400.0f;
+    static float last_y = 300.0f;
+    if (first_mouse)
+    {
+        last_x = x;
+        last_y = y;
+        first_mouse = false;
+        return;
+    }
+    float x_offset = x - last_x;
+    float y_offset = last_y - y;
+    last_x = x;
+    last_y = y;
+    ProcessMouseMovement(camera, x_offset, y_offset, true);
+}
+
+void
+scroll_callback(GLFWwindow *window, double x_offset, double y_offset)
+{
+    ProcessMouseScroll(camera, (float)y_offset);
+}
+
+int
+main()
+{
+    GLFWwindow *window;
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    if (glewInit() != GLEW_OK)
+    {
+        printf("[GLEW ERROR]: failed to init glew\n");
+    }
+    printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
+
+    glViewport(0, 0, width, height);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    Texture container_texture = {};
+    stbi_set_flip_vertically_on_load(true);
+    container_texture.data = stbi_load("assets/container2.png", &container_texture.width, &container_texture.height, &container_texture.channels, 0);
+    if (!container_texture.data)
+    {
+        printf("[STB_Image ERROR]: failed to load image\n");
+        return -1;
+    }
+    glGenTextures(1, &container_texture.id);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, container_texture.id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, container_texture.width, container_texture.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, container_texture.data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(container_texture.data);
+
+    Texture container_metal_outline_texture = {};
+    stbi_set_flip_vertically_on_load(true);
+    container_metal_outline_texture.data = stbi_load("assets/container2_specular.png",
+                                           &container_metal_outline_texture.width,
+                                           &container_metal_outline_texture.height,
+                                           &container_metal_outline_texture.channels, 0);
+    if (!container_metal_outline_texture.data)
+    {
+        printf("[STB_Image ERROR]: failed to load image\n");
+        return -1;
+    }
+    glGenTextures(1, &container_metal_outline_texture.id);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, container_metal_outline_texture.id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, container_metal_outline_texture.width, container_metal_outline_texture.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, container_metal_outline_texture.data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(container_metal_outline_texture.data);
+
+    // bind vertex array
+    unsigned int vertex_array_id;
+    glGenVertexArrays(1, &vertex_array_id);
+    glBindVertexArray(vertex_array_id);
+
+    // copy vertices array in a vertex buffer
+    float vertices[] = {
+        // positions          // normals           // texture coords
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
+    };
+
+    unsigned vertex_buffer_id;
+    glGenBuffers(1, &vertex_buffer_id);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_id);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // set vertex arrtribute pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    unsigned int vertex_array_light_id;
+    glGenVertexArrays(1, &vertex_array_light_id);
+    glBindVertexArray(vertex_array_light_id);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_id);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    ShaderProgram *shader_program = CreateShaderProgram("shaders/vertex.glsl", "shaders/fragment.glsl");   
+    if (!shader_program)
+    {
+        printf("[SHADER ERROR]: failed to load base shader\n");
+    }
+
+    ShaderProgram *light_shader_program = CreateShaderProgram("shaders/vertex_light_source.glsl", "shaders/fragment_light_source.glsl");
+    if (!light_shader_program)
+    {
+        printf("[SHADER ERROR]: failed to load light shader\n");
+    }
+
+    glBindVertexArray(0);
+    glEnable(GL_DEPTH_TEST);
+
+    glm::vec3 light_position(0.0f, 1.0f, 1.5f);
+    camera = CreateCamera(camera_position, camera_up, yaw, pitch);
+
+    glm::vec3 cube_position = glm::vec3(0.0f, 0.0f, 0.0f);
+
+    glUseProgram(shader_program->id);
+    SetIntUniform(shader_program, "u_material.diffuse", 0);
+    SetIntUniform(shader_program, "u_material.specular", 1);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        process_input(window);
+        {
+            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            if (WireFrameEnabled)
+            {
+                glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+            }
+            else
+            {
+                glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+            }
+            glm::mat4 view = glm::mat4(1.0f);
+            view = CustomGetViewMatrix(camera);
+            glm::mat4 perspective_projection = glm::perspective(glm::radians(camera->zoom), (float)width / (float)height, 0.1f, 100.0f);
+            glm::mat4 model = glm::mat4(1.0f);
+
+            // render light source
+            glUseProgram(light_shader_program->id);
+            glBindVertexArray(vertex_array_light_id);
+            model = glm::mat4(1.0f);
+            static float x = 0.0f;
+            static float increment_amount = -2.0f;
+            x += increment_amount * delta_time;
+            if (x < -2.0f || x > 2.0f)
+            {
+                increment_amount *= -1.0f;
+            }
+            light_position.x = x;
+            model = glm::translate(model, light_position);
+            model = glm::scale(model, glm::vec3(0.2f));
+            SetFloatMat4Uniform(light_shader_program, "u_model", glm::value_ptr(model));
+            SetFloatMat4Uniform(light_shader_program, "u_view", glm::value_ptr(view));
+            SetFloatMat4Uniform(light_shader_program, "u_projection", glm::value_ptr(perspective_projection));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+
+            // render cubes
+            glUseProgram(shader_program->id);
+            glBindVertexArray(vertex_array_id);
+
+            SetFloatUniform(shader_program, "u_material.shininess", 64.0f);
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, container_texture.id);
+            glActiveTexture(GL_TEXTURE1);
+            glBindTexture(GL_TEXTURE_2D, container_metal_outline_texture.id);
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, cube_position);
+            SetFloatMat4Uniform(shader_program, "u_model", glm::value_ptr(model));
+            SetFloatMat4Uniform(shader_program, "u_view", glm::value_ptr(view));
+            SetFloatMat4Uniform(shader_program, "u_projection", glm::value_ptr(perspective_projection));
+            SetFloatVec3Uniform(shader_program, "u_view_position", camera->position);
+            SetFloatVec3Uniform(shader_program, "u_light.position", light_position);
+            SetFloatVec3Uniform(shader_program, "u_light.ambient", glm::vec3(0.2f, 0.2f, 0.2f));
+            SetFloatVec3Uniform(shader_program, "u_light.diffuse", glm::vec3(0.5f, 0.5f, 0.5f));
+            SetFloatVec3Uniform(shader_program, "u_light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+
+            float current_frame = glfwGetTime();
+            delta_time = current_frame - last_frame;
+            last_frame = current_frame;
+        }
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    
+    {
+        ReleaseShaderProgram(shader_program);
+        ReleaseShaderProgram(light_shader_program);
+        glDeleteVertexArrays(1, &vertex_array_id);
+        glDeleteBuffers(1, &vertex_buffer_id);
+        glfwTerminate();
+    }
+    
+    return 0;
+}
