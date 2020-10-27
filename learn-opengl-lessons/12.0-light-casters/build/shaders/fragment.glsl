5d652b3b2258b9c8896231ce9410454229921e73
Nicholas Kane <nkanedev@gmail.com>
2020-10-27 07:49:05 -0400

    wip

---
+++ learn-opengl-lessons/12.0-light-casters/build/shaders/fragment.glsl
@@ -0,0 +1,47 @@
+#version 330 core
+
+struct Material 
+{
+    sampler2D diffuse;
+    sampler2D specular;
+    float     shininess;
+};
+
+struct Light
+{
+    vec3 direction;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+out vec4 v_frag_color;
+
+in vec3 v_fragment_position;
+in vec3 v_normal;
+in vec2 v_texture_coordinates;
+
+uniform vec3 u_view_position;
+uniform Material u_material;
+uniform Light u_light;
+
+void main()
+{
+    // ambient
+    vec3 ambient = u_light.ambient * texture(u_material.diffuse, v_texture_coordinates).rgb;
+
+    // diffuse
+    vec3 norm = normalize(v_normal);
+    vec3 light_direction = normalize(-u_light.direction);
+    float diff = max(dot(norm, light_direction), 0.0);
+    vec3 diffuse = u_light.diffuse * diff * texture(u_material.diffuse, v_texture_coordinates).rgb;
+
+    // specular
+    vec3 view_direction = normalize(u_view_position - v_fragment_position);
+    vec3 reflection_direction = reflect(-light_direction, norm);
+    float spec = pow(max(dot(view_direction, reflection_direction), 0.0), u_material.shininess);
+    vec3 specular = u_light.specular * spec * texture(u_material.specular, v_texture_coordinates).rgb;
+
+    vec3 result = ambient + diffuse + specular;
+    v_frag_color = vec4(result, 1.0);
+}
