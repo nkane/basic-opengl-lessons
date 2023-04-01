dbcfb898a8cd01fb2a026ce43e1004bcb0e6af85
Nicholas Kane <nkanedev@gmail.com>
2023-04-01 19:30:34 -0400

    splitting triangle and square example

--- learn-opengl-lessons/01-hello-window/main.cpp
+++ learn-opengl-lessons/01-hello-window/main.cpp
@@ -28,6 +28,7 @@ main()
         return -1;
     }
 
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
--- learn-opengl-lessons/02.0-hello-triangle/build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if not exist build mkdir build
-pushd build
-cl /nologo /Zi /MD ..\main.cpp /I..\..\libs\include /link ..\..\libs\glfw\lib-vc2019\glfw3.lib ..\..\libs\glew\glew32s.lib user32.lib gdi32.lib shell32.lib opengl32.lib 
-popd
